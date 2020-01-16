9e1bf66c90b3c15c1ca398ced2a08789e9ec3173
Ahmad Fariz <fariz@voltras.co.id>
2020-01-16 15:56:48 +0700

    upload my robofriends project to github

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Include Buddy',
+        username: 'includebuddy',
+        email: 'includebuddy@mail.com'
+    },
+    {
+        id: 2,
+        name: 'Johnny William',
+        username: 'johnnywilliam',
+        email: 'johnnywilliam@mail.com'
+    },
+    {
+        id: 3,
+        name: 'Ace Hardware',
+        username: 'acehardware',
+        email: 'acehardware@mail.com'
+    },
+    {
+        id: 4,
+        name: 'Permata Volunteerism',
+        username: 'permatavolunteerism',
+        email: 'permatavolunteerism@mail.com'
+    }
+]
