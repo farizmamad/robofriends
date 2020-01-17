bffa40d901c685b85d8a8e3d65eda9d4bcf2b6cf
Ahmad Fariz <fariz@voltras.co.id>
2020-01-17 18:02:42 +0700

    update codes, using states and fetch api

--- src/CardList.js
+++ src/CardList.js
@@ -5,14 +5,14 @@ const CardList = ({ robots }) => {
     return(
         <div>
             {
-                robots.map((user,i) => {
+                robots.map((user, i) => {
                     return (
                         <Card 
                             key={robots[i].id} 
                             id={robots[i].id} 
                             name={robots[i].name} 
                             email={robots[i].email} 
-                            />
+                        />
                     );
                 })
             }
